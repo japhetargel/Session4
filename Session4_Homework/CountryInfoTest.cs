25054aa3b18ff79211f4d26172ffbab69984a769
Japhet Argel <markjaphet.argel@cognizant.com>
2022-10-19 17:38:52 +0800

    Session 4 Homework

---
+++ Session4_Homework/CountryInfoTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Session4_Homework
+{
+    [TestClass]
+    public class CountryInfoServiceTests
+    {
+        public readonly ServiceReference1.CountryInfoServiceSoapTypeClient countryInfo =
+            new ServiceReference1.CountryInfoServiceSoapTypeClient(ServiceReference1.CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+
+        [TestMethod]
+        public void ListOfCountryNamesByCode()
+        {
+            var listOfCountryNamesByCode = countryInfo.ListOfCountryNamesByCode();
+            var listOfCountryNamesResult = listOfCountryNamesByCode.OrderBy(a => a.sISOCode);
+            var isAscending = listOfCountryNamesByCode.SequenceEqual(listOfCountryNamesResult);
+            Assert.IsTrue(isAscending);
+        }
+
+        [TestMethod]
+        public void ValidatePassInvalidCountryCode()
+        {
+            var countryCode = "usa1123";
+            var response = countryInfo.CountryName(countryCode);
+            Assert.IsTrue(response.Contains("Country not found in the database"), $"Country code {countryCode} can be found in the database.");
+        }
+
+        [TestMethod]
+        public void ValidateLastCountryName()
+        {
+            var countryList = countryInfo.ListOfCountryNamesByCode();
+            var lastCountry = countryList.LastOrDefault();
+            var country = countryInfo.CountryName(lastCountry.sISOCode);
+            Assert.AreEqual(lastCountry.sName, country);
+        }
+    }
+}
